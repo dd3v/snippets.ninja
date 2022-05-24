50ddfc26ae926a732bfe82240dfeee97d382b5b4
Dmitry Magalyas <dmitry.magalyas@gmail.com>
2022-05-24 22:22:07 +0300

    reset.css

--- src/main.js
+++ src/main.js
@@ -2,9 +2,6 @@ import { createApp } from 'vue';
 import UIcon from './components/base/UIcon.vue';
 import App from './App.vue';
 import './assets/css/app.css';
-import './assets/css/fonts.css';
-import './assets/css/icons.css';
-
 
 const app = createApp(App);
 app.component('u-icon', UIcon);
